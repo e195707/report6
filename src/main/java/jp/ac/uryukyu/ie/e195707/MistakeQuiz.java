d0ee5c3112d000ef5cdb7738bff1c6df2169bf08
Yuta Tomiyama <e195707@ie.u-ryukyu.ac.jp>
2020-01-13 01:39:44 +0900

    全体像

---
+++ src/main/java/jp/ac/uryukyu/ie/e195707/MistakeQuiz.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e195707;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+class MistakeQuiz{
+    String question;
+    String answer;
+
+    MistakeQuiz(){
+        createQuiz();
+    }
+
+    void createQuiz(){
+    }
+    void QuizPlay(){
+        
+    }
+}
+
